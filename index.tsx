037693750e2453c19569c521e7032a45ac39d013
PROTEX <94780381+al-redowan@users.noreply.github.com>
2025-08-03 00:19:06 +0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-
+import { Analytics } from "@vercel/analytics/remix"
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
